65ec802b8ba387e05b2c4529252b1bae43462f0f
liyujiang <1032694760@qq.com>
2020-08-17 10:03:56 +0800

    动态获取外置存储根路径

--- app/src/main/java/io/github/rosemoe/editor/utils/CrashHandler.java
+++ app/src/main/java/io/github/rosemoe/editor/utils/CrashHandler.java
@@ -163,12 +163,12 @@ public class CrashHandler implements UncaughtExceptionHandler {
             String time = formatter.format(new Date());
             String fileName = "crash-" + time + "-" + timestamp + ".log";
             if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                String path = "/sdcard/#Logs/";
+                String path = Environment.getExternalStoragePublicDirectory("#Logs").getAbsolutePath();
                 File dir = new File(path);
                 if (!dir.exists())
                     dir.mkdirs();
 
-                FileOutputStream fos = new FileOutputStream(path + fileName);
+                FileOutputStream fos = new FileOutputStream(new File(path, fileName));
 //				FileOutputStream fos = mContext.openFileOutput(fileName, Context.MODE_PRIVATE);
                 fos.write(sb.toString().getBytes());
                 Log.e("crash", sb.toString());
