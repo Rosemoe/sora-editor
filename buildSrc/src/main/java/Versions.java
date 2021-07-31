17c0d9703695aa2e688af9730ca91c959ae0271d
Rosemoe <2073412493@qq.com>
2021-07-31 21:28:56 +0800

    Remove access to debug logs Update version name

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -21,8 +21,8 @@
 public final class Versions {
 
     // Project versions
-    public final static String versionName = "0.5.2";
-    public final static int versionCode = 10;
+    public final static String versionName = "0.5.3";
+    public final static int versionCode = 11;
 
     // Platform & Tool versions
     public final static String buildToolsVersion = "30.0.3";
