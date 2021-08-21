617e294eb537432286435178496b30e914d9ffaf
Rosemoe <2073412493@qq.com>
2021-08-21 14:39:52 +0800

    Add Tab key to select auto-completion item & Update LICENSE profile and copyrights

--- language-python/src/main/java/io/github/rosemoe/editor/langs/python/PythonLexer.java
+++ language-python/src/main/java/io/github/rosemoe/editor/langs/python/PythonLexer.java
@@ -2,20 +2,22 @@
  *    CodeEditor - the awesome code editor for Android
  *    Copyright (C) 2020-2021  Rosemoe
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
+ *     This library is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *     USA
  *
- *     Please contact Rosemoe by email roses2020@qq.com if you need
+ *     Please contact Rosemoe by email 2073412493@qq.com if you need
  *     additional information or have any questions
  */
 package io.github.rosemoe.editor.langs.python;// Generated from python\PythonLexer.g4 by ANTLR 4.9.2
