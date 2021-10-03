c51085027a1fbcee934176ee6601bb325c32aaf0
Rosemoe <2073412493@qq.com>
2021-10-03 12:13:43 +0800

    Update version name to 0.6.0-dev-4 Fix unspecified dependency item in generated pom file

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -24,8 +24,8 @@
 public final class Versions {
 
     // Project versions
-    public final static String versionName = "0.6.0-dev-3";
-    public final static int versionCode = 16;
+    public final static String versionName = "0.6.0-dev-4";
+    public final static int versionCode = 17;
 
     // Platform & Tool versions
     public final static String buildToolsVersion = "31.0.0";
