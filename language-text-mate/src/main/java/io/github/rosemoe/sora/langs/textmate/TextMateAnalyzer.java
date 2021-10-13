996895f4689440e540dd765adbf0085c35133f69
Coyamo <1320062779@qq.com>
2021-10-14 00:36:46 +0800

    Optimize the use of theme highlighting. Now the color display is correct

--- language-text-mate/src/main/java/io/github/rosemoe/sora/langs/textmate/TextMateAnalyzer.java
+++ language-text-mate/src/main/java/io/github/rosemoe/sora/langs/textmate/TextMateAnalyzer.java
@@ -25,6 +25,7 @@ package io.github.rosemoe.sora.langs.textmate;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -39,7 +40,9 @@ import io.github.rosemoe.sora.text.TextAnalyzer;
 import io.github.rosemoe.sora.textmate.core.grammar.IGrammar;
 import io.github.rosemoe.sora.textmate.core.grammar.IToken;
 import io.github.rosemoe.sora.textmate.core.grammar.ITokenizeLineResult;
+import io.github.rosemoe.sora.textmate.core.grammar.ITokenizeLineResult2;
 import io.github.rosemoe.sora.textmate.core.grammar.StackElement;
+import io.github.rosemoe.sora.textmate.core.internal.grammar.StackElementMetadata;
 import io.github.rosemoe.sora.textmate.core.registry.Registry;
 import io.github.rosemoe.sora.textmate.core.theme.FontStyle;
 import io.github.rosemoe.sora.textmate.core.theme.IRawTheme;
@@ -49,8 +52,8 @@ import io.github.rosemoe.sora.widget.EditorColorScheme;
 
 public class TextMateAnalyzer implements CodeAnalyzer {
 
-    private Registry registry = new Registry();
-    private IGrammar grammar;
+    private final Registry registry = new Registry();
+    private final IGrammar grammar;
     private Theme theme;
 
     public TextMateAnalyzer(String grammarName, InputStream grammarIns, IRawTheme theme) throws Exception {
@@ -75,56 +78,34 @@ public class TextMateAnalyzer implements CodeAnalyzer {
                     result.addNormalIfNull();
                     first = false;
                 }
-                ITokenizeLineResult lineTokens = grammar.tokenizeLine(line, ruleStack);
-                ruleStack = lineTokens.getRuleStack();
-                for (int i = 0; i < lineTokens.getTokens().length; i++) {
-                    IToken token = lineTokens.getTokens()[i];
-                    List<ThemeTrieElementRule> list = theme.match(token.getScopes().get(token.getScopes().size() - 1));
-                    ThemeTrieElementRule rule=list.get(0);
+                ITokenizeLineResult2 lineTokens = grammar.tokenizeLine2(line, ruleStack);
+                int tokensLength = lineTokens.getTokens().length / 2;
+                for (int i = 0; i < tokensLength; i++) {
+                    int startIndex = lineTokens.getTokens()[2 * i];
+			        int nextStartIndex = i + 1 < tokensLength ? lineTokens.getTokens()[2 * i + 2] : line.length();
+			        String tokenText = line.substring(startIndex, nextStartIndex);
+                    if (tokenText.trim().isEmpty()) {
+                        continue;
+                    }
+			        int metadata = lineTokens.getTokens()[2 * i + 1];
+			        int foreground = StackElementMetadata.getForeground(metadata);
+			        int fontStyle=StackElementMetadata.getFontStyle(metadata);
+                    Span span=Span.obtain(startIndex, foreground + 255);
 
-                    Span span=Span.obtain(token.getStartIndex(), rule.foreground + 255);
-
-                    if(rule.fontStyle!=FontStyle.NotSet){
-                        if((rule.fontStyle&FontStyle.Underline)== FontStyle.Underline){
-                            String color=theme.getColor(rule.foreground);
+                    if(fontStyle!=FontStyle.NotSet){
+                        if((fontStyle&FontStyle.Underline)== FontStyle.Underline){
+                            String color=theme.getColor(foreground);
                             if(color!=null){
                                 span.underlineColor=Color.parseColor(color);
                             }
 
                         }
-                        //Paint object should not be modified. This is just for test.
-                        span.renderer=new ExternalRenderer() {
-                            @Override
-                            public boolean requirePreDraw() {
-                                return true;
-                            }
 
-                            @Override
-                            public boolean requirePostDraw() {
-                                return true;
-                            }
-
-                            final boolean bold=(rule.fontStyle&FontStyle.Bold)== FontStyle.Bold;
-                            final boolean italic=(rule.fontStyle&FontStyle.Italic)== FontStyle.Italic;
-                            @Override
-                            public void draw(Canvas canvas, Paint paint, EditorColorScheme colorScheme, boolean preOrPost) {
-                                if(bold){
-                                    paint.setFakeBoldText(preOrPost);
-                                }
-
-                                if(italic){
-                                    paint.setTextSkewX(preOrPost?-0.2f:0);
-                                }
-                            }
-                        };
                     }
-
-
-
 
                     result.add(lineCount,span);
                 }
-
+                ruleStack = lineTokens.getRuleStack();
                 lineCount++;
             }
 
@@ -139,6 +120,5 @@ public class TextMateAnalyzer implements CodeAnalyzer {
     protected void updateTheme(IRawTheme theme) {
         registry.setTheme(theme);
         this.theme = Theme.createFromRawTheme(theme);
-        ;
     }
 }
