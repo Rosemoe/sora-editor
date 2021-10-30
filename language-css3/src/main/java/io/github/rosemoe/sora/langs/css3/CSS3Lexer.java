4e4faaa18f866e204aaed79861cc4aea3f874902
Rosemoe <2073412493@qq.com>
2021-10-30 16:59:14 +0800

    Update copyright

--- language-css3/src/main/java/io/github/rosemoe/sora/langs/css3/CSS3Lexer.java
+++ language-css3/src/main/java/io/github/rosemoe/sora/langs/css3/CSS3Lexer.java
@@ -1,3 +1,27 @@
+/*
+ *    sora-editor - the awesome code editor for Android
+ *    https://github.com/Rosemoe/CodeEditor
+ *    Copyright (C) 2020-2021  Rosemoe
+ *
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
+ *
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *     USA
+ *
+ *     Please contact Rosemoe by email 2073412493@qq.com if you need
+ *     additional information or have any questions
+ */
+
 // Generated from D:/Desktop/CodeEditor/language-css3/src/main/java/io/github/rosemoe/sora/langs/css3\css3.g4 by ANTLR 4.9.1
 package io.github.rosemoe.sora.langs.css3;
 
