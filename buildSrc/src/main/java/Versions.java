5ca7c9b46950772b8d29095a435ce5385da31141
Rosemoe <2073412493@qq.com>
2021-10-30 15:31:25 +0800

    Bump version to 0.8.2

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -24,8 +24,8 @@
 public final class Versions {
 
     // Project versions
-    public final static String versionName = "0.8.1";
-    public final static int versionCode = 23;
+    public final static String versionName = "0.8.2";
+    public final static int versionCode = 24;
 
     // Platform & Tool versions
     public final static String buildToolsVersion = "31.0.0";
