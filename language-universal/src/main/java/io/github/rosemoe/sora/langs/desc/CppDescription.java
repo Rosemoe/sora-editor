62eae00ae28fe6113909de5b801a0782fbb2fb81
Rosemoe <2073412493@qq.com>
2021-10-30 13:07:48 +0800

    Update CppDescription.java

--- language-universal/src/main/java/io/github/rosemoe/sora/langs/desc/CppDescription.java
+++ language-universal/src/main/java/io/github/rosemoe/sora/langs/desc/CppDescription.java
@@ -41,7 +41,7 @@ public class CppDescription extends CDescription {
                 "struct", "switch", "template", "this", "throw", "true", "try",
                 "typedef", "typeid", "typename", "unsigned", "union",
                 "using", "virtual", "void", "volatile", "wchar_t", "while", "define","include",
-          "_Bool", "char16_t","char32_t","cl","cin","do"
+                "_Bool", "char16_t","char32_t"
         };
     }
 }
