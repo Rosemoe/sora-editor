0fc9fc6a4b018b523cb5bc0985db1b24de75542c
ninjacoder <89408667+Ninjacoderhsi@users.noreply.github.com>
2021-11-20 10:55:41 +0330

    Create AndroidXmlDescription.java

---
+++ language-universal/src/main/java/io/github/rosemoe/sora/langs/desc/AndroidXmlDescription.java
@@ -0,0 +1,100 @@
+/*
+ *    sora-editor - the awesome code editor for Android
+ *    https://github.com/Rosemoe/CodeEditor
+ *    Copyright (C) 2020-2021  Rosemoe
+ *
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
+ *
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *     USA
+ *
+ *     Please contact Rosemoe by email 2073412493@qq.com if you need
+ *     additional information or have any questions
+ */
+package io.github.rosemoe.sora.langs.desc;
+
+import io.github.rosemoe.sora.langs.universal.LanguageDescription;
+
+/**
+ * @author Rose
+ */
+public class AndroidXmlDescription implements LanguageDescription {
+
+    @Override
+    public boolean isOperator(char[] characters, int length) {
+        if (length == 1) {
+            char c = characters[0];
+            return (c == '+' || c == '-' || c == '{' || c == '}' || c == '[' || c == ']' ||
+                    c == '(' || c == ')' || c == '|' || c == ':' || c == '.' || c == ',' ||
+                    c == ';' || c == '*' || c == '/' || c == '&' || c == '^' || c == '%' ||
+                    c == '!' || c == '~' || c == '<' || c == '>' || c == '=' || c == '\\' ||
+                    c == '#');
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isLineCommentStart(char a, char b) {
+        return a == '<' && b == '!';
+    }
+
+    @Override
+    public boolean isLongCommentStart(char a, char b) {
+        return a == '<' && b == '-';
+    }
+
+    @Override
+    public boolean isLongCommentEnd(char a, char b) {
+        return a == '-' && b == '>';
+    }
+
+    @Override
+    public String[] getKeywords() {
+        //noinspection SpellCheckingInspection
+        return new String[]{
+                "scaleType", "layout_height", "layout_width","layout_gravity","src","orientation",
+                "layout_weight","layout_marginBottom","padding","gravity","background"
+        };
+    }
+
+    @Override
+    public boolean useTab() {
+        return false;
+    }
+
+    @Override
+    public int getOperatorAdvance(String operator) {
+        switch (operator) {
+            case "<":
+                return 4;
+            case ">":
+                return -4;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean isSupportBlockLine() {
+        return true;
+    }
+
+    @Override
+    public boolean isBlockStart(String operator) {
+        return operator.equals("<");
+    }
+
+    @Override
+    public boolean isBlockEnd(String operator) {
+        return operator.equals("/>");
+    }
+}
