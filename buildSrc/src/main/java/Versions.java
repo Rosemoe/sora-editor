e9b5c53ce530af7c2fb738e69a1205c737fc6f64
Rosemoe <2073412493@qq.com>
2021-11-26 22:58:20 +0800

    Update sdk version

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -25,11 +25,11 @@
 public final class Versions {
 
     // Project versions
-    public final static String versionName = "0.8.4";
-    public final static int versionCode = 26;
+    public final static String versionName = "0.9.0-dev-1";
+    public final static int versionCode = 27;
 
     // Platform & Tool versions
-    public final static String buildToolsVersion = "31.0.0";
+    public final static String buildToolsVersion = "32.0.0-rc1";
     public final static int compileSdkVersion = 31;
     public final static int minSdkVersion = 21;
     public final static int targetSdkVersion = 31;
