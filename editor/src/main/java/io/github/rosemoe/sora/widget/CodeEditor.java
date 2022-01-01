0afdaaeffadd3d9725cc9b9c66e1a6de02d6e7de
Rosemoe <2073412493@qq.com>
2022-01-02 00:22:05 +0800

    Fix selected text background

--- editor/src/main/java/io/github/rosemoe/sora/widget/CodeEditor.java
+++ editor/src/main/java/io/github/rosemoe/sora/widget/CodeEditor.java
@@ -1987,7 +1987,8 @@ public class CodeEditor extends View implements ContentListener, TextAnalyzer.Ca
      * @param color          Color of background
      */
     protected void drawRowRegionBackground(Canvas canvas, float paintingOffset, int row, int firstVis, int lastVis, int highlightStart, int highlightEnd, int color, int line) {
-        int paintStart = Math.min(Math.max(Math.max(0, firstVis - 1), highlightStart), lastVis);
+        firstVis = Math.max(0, firstVis - 1);
+        int paintStart = Math.min(Math.max(firstVis, highlightStart), lastVis);
         int paintEnd = Math.min(Math.max(firstVis, highlightEnd), lastVis);
         if (paintStart != paintEnd) {
             mRect.top = getRowTop(row) - getOffsetY();
