65acd1a36aa9c28f191fce19e4ae836ea02f8b19
Rosemoe <2073412493@qq.com>
2022-02-05 16:08:55 +0800

    Bump version to 0.10.0

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -25,8 +25,8 @@
 public final class Versions {
 
     // Project versions
-    public final static String versionName = "0.9.3";
-    public final static int versionCode = 34;
+    public final static String versionName = "0.10.0";
+    public final static int versionCode = 35;
 
     // Platform & Tool versions
     public final static String buildToolsVersion = "32.0.0";
