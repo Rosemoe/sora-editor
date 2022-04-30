0c50d17bbac4e2732dae89506c67287f3d3e0d5c
Rosemoe <2073412493@qq.com>
2022-04-30 21:48:37 +0800

    Degrade build tool version for CI runs

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -29,7 +29,7 @@ public final class Versions {
     public final static int versionCode = 47;
 
     // Platform & Tool versions
-    public final static String buildToolsVersion = "33.0.0-rc3";
+    public final static String buildToolsVersion = "32.0.0";
     public final static int compileSdkVersion = 32;
     public final static int minSdkVersion = 21;
     public final static int targetSdkVersion = 32;
