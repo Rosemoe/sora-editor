c54549bab65fb0d341396d1cce6d448162b6c16a
Rosemoe <2073412493@qq.com>
2022-07-24 17:55:15 +0800

    Add more checks when committing symbols to editor

--- editor/src/main/java/io/github/rosemoe/sora/widget/SymbolInputView.java
+++ editor/src/main/java/io/github/rosemoe/sora/widget/SymbolInputView.java
@@ -119,7 +119,7 @@ public class SymbolInputView extends LinearLayout {
             addView(btn, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT));
             int finalI = i;
             btn.setOnClickListener((view) -> {
-                if (editor != null)
+                if (editor != null && editor.isEditable() && !editor.isFormatting())
                     editor.insertText(insertText[finalI], 1);
             });
         }
