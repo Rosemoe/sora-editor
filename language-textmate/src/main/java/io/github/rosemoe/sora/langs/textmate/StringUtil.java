3ead826f64f2472a587ea9793ea35ed8658b6811
Rosemoe <2073412493@qq.com>
2022-08-15 20:27:46 +0800

    Simplify code

--- language-textmate/src/main/java/io/github/rosemoe/sora/langs/textmate/StringUtil.java
+++ language-textmate/src/main/java/io/github/rosemoe/sora/langs/textmate/StringUtil.java
@@ -26,7 +26,7 @@ package io.github.rosemoe.sora.langs.textmate;
 public class StringUtil {
 
     public static boolean checkSurrogate(String text) {
-        for (int i = 0;i < text.length();i++) {
+        for (int i = 0; i < text.length(); i++) {
             if (Character.isSurrogate(text.charAt(i))) {
                 return true;
             }
@@ -37,21 +37,15 @@ public class StringUtil {
     public static int convertUnicodeOffsetToUtf16(String text, int offset, boolean hasSurrogate) {
         if (hasSurrogate) {
             var j = 0;
-            for (int i = 0;i < text.length();i++) {
+            for (int i = 0; i < text.length(); i++) {
                 if (j == offset) {
                     return i;
                 }
                 var ch = text.charAt(i);
-                if (Character.isHighSurrogate(ch)) {
-                    if (i + 1 < text.length() && Character.isLowSurrogate(text.charAt(i + 1))) {
-                        i++;
-                        j++;
-                    } else {
-                        j++;
-                    }
-                } else {
-                    j++;
+                if (Character.isHighSurrogate(ch) && i + 1 < text.length() && Character.isLowSurrogate(text.charAt(i + 1))) {
+                    i++;
                 }
+                j++;
             }
         }
         return offset;
