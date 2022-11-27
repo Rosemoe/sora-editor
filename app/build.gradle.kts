4cea29374cb8c4f52c892386ce92fb55965c0405
Rosemoe <2073412493@qq.com>
2022-11-28 00:28:27 +0800

    feat: attach leak canary to debug sample app builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,7 +107,7 @@ dependencies {
     //Xml language server
     implementation("org.eclipse.lemminx:org.eclipse.lemminx:0.17.0")
 
-
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.10")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
