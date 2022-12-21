8f652a618f0722f3ff92057087bf2c241f9e1cac
Rosemoe <2073412493@qq.com>
2022-12-21 20:36:25 +0800

    chore: bump version to 0.20.1

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -25,8 +25,8 @@
 public final class Versions {
 
     // Project versions
-    public final static String versionName = "0.20.0";
-    public final static int versionCode = 71;
+    public final static String versionName = "0.20.1";
+    public final static int versionCode = 72;
 
     // Platform & Tool versions
     public final static String buildToolsVersion = "33.0.0";
