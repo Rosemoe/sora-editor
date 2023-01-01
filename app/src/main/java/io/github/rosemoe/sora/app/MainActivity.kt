6e166c3c0e7e91c98c1e80d25354e8515886335c
Rosemoe <2073412493@qq.com>
2023-01-01 10:57:25 +0800

    chore: update copyright

--- app/src/main/java/io/github/rosemoe/sora/app/MainActivity.kt
+++ app/src/main/java/io/github/rosemoe/sora/app/MainActivity.kt
@@ -1,7 +1,7 @@
-/*
+/*******************************************************************************
  *    sora-editor - the awesome code editor for Android
  *    https://github.com/Rosemoe/sora-editor
- *    Copyright (C) 2020-2022  Rosemoe
+ *    Copyright (C) 2020-2023  Rosemoe
  *
  *     This library is free software; you can redistribute it and/or
  *     modify it under the terms of the GNU Lesser General Public
@@ -20,7 +20,7 @@
  *
  *     Please contact Rosemoe by email 2073412493@qq.com if you need
  *     additional information or have any questions
- */
+ ******************************************************************************/
 package io.github.rosemoe.sora.app
 
 import android.app.AlertDialog
--- editor/src/main/java/io/github/rosemoe/sora/text/ContentReference.java
+++ editor/src/main/java/io/github/rosemoe/sora/text/ContentReference.java
@@ -1,7 +1,7 @@
 /*
  *    sora-editor - the awesome code editor for Android
  *    https://github.com/Rosemoe/sora-editor
- *    Copyright (C) 2020-2022  Rosemoe
+ *    Copyright (C) 2020-2023  Rosemoe
  *
  *     This library is free software; you can redistribute it and/or
  *     modify it under the terms of the GNU Lesser General Public
