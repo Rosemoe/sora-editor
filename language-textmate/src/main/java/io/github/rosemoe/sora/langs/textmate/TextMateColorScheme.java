5f8f8aa12baef822f6851d9d68bed819ff725ae6
Pranav <purwarpranav80@gmail.com>
2023-08-07 20:24:28 +0530

    Add support for setting tooltip colors with textmate theme

--- language-textmate/src/main/java/io/github/rosemoe/sora/langs/textmate/TextMateColorScheme.java
+++ language-textmate/src/main/java/io/github/rosemoe/sora/langs/textmate/TextMateColorScheme.java
@@ -183,6 +183,26 @@ public class TextMateColorScheme extends EditorColorScheme implements ThemeRegis
             setColor(HIGHLIGHTED_DELIMITERS_FOREGROUND, Color.parseColor(highlightedDelimetersForeground));
         }
 
+        String tooltipBackground = (String) themeRaw.get("tooltipBackground");
+        if (tooltipBackground != null) {
+            setColor(DIAGNOSTIC_TOOLTIP_BACKGROUND, Color.parseColor(tooltipBackground));
+        }
+
+        String tooltipBriefMessageColor = (String) themeRaw.get("tooltipBriefMessageColor");
+        if (tooltipBriefMessageColor != null) {
+            setColor(DIAGNOSTIC_TOOLTIP_BRIEF_MSG, Color.parseColor(tooltipBriefMessageColor));
+        }
+
+        String tooltipDetailedMessageColor = (String) themeRaw.get("tooltipDetailedMessageColor");
+        if (tooltipDetailedMessageColor != null) {
+            setColor(DIAGNOSTIC_TOOLTIP_DETAILED_MSG, Color.parseColor(tooltipDetailedMessageColor));
+        }
+
+        String tooltipActionColor = (String) themeRaw.get("tooltipActionColor");
+        if (tooltipActionColor != null) {
+            setColor(DIAGNOSTIC_TOOLTIP_ACTION, Color.parseColor(tooltipActionColor));
+        }
+
 
         String editorIndentGuideBackground = (String) themeRaw.get("editorIndentGuide.background");
         int blockLineColor = ((getColor(WHOLE_BACKGROUND) + getColor(TEXT_NORMAL)) / 2) & 0x00FFFFFF | 0x88000000;
