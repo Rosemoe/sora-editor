dae64acb020947283c08d7696825e5a15dfdbd4f
Akash Yadav <itsaky01@gmail.com>
2023-09-08 13:33:11 +0530

    fix: add check for text selection before indenting selection in symbol input view

--- editor/src/main/java/io/github/rosemoe/sora/widget/SymbolInputView.java
+++ editor/src/main/java/io/github/rosemoe/sora/widget/SymbolInputView.java
@@ -128,7 +128,7 @@ public class SymbolInputView extends LinearLayout {
                 if ("\t".equals(insertText[finalI])) {
                     if (editor.getSnippetController().isInSnippet()) {
                         editor.getSnippetController().shiftToNextTabStop();
-                    } else if (editor.getProps().indentSelectionWithTab) {
+                    } else if (editor.getProps().indentSelectionWithTab && editor.getCursor().isSelected()) {
                         editor.indentSelection();
                     } else {
                         editor.insertText(insertText[finalI], 1);
