d0c93684a5644ca6b7c1aa5d4edb80f5aba71955
Rosemoe <2073412493@qq.com>
2024-02-29 17:29:39 +0800

    tests: add some test case

---
+++ editor/src/test/java/io/github/rosemoe/sora/text/ContentIOTest.kt
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ *    sora-editor - the awesome code editor for Android
+ *    https://github.com/Rosemoe/sora-editor
+ *    Copyright (C) 2020-2024  Rosemoe
+ *
+ *     This library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
+ *
+ *     This library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this library; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *     USA
+ *
+ *     Please contact Rosemoe by email 2073412493@qq.com if you need
+ *     additional information or have any questions
+ ******************************************************************************/
+
+package io.github.rosemoe.sora.text
+
+import org.junit.Test
+import java.io.StringWriter
+import com.google.common.truth.Truth.assertThat
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE)
+class ContentIOTest {
+
+    @Test
+    fun testWriter() {
+        val text = "Hello\nWorld\r\nTest\rTest"
+        val content = Content(text)
+        val writer = StringWriter()
+        ContentIO.writeTo(content, writer, true)
+        assertThat(writer.buffer.toString()).isEqualTo(text)
+    }
+
+}
