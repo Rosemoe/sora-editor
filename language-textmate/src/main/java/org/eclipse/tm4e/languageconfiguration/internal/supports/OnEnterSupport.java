e4714e66773c33d1e5657d16ce251204827889d4
dingyi222666 <dingyi222666@foxmail.com>
2024-05-16 15:09:25 +0800

    fix(lang-textmate): compatibility issue under API 34

--- language-textmate/src/main/java/org/eclipse/tm4e/languageconfiguration/internal/supports/OnEnterSupport.java
+++ language-textmate/src/main/java/org/eclipse/tm4e/languageconfiguration/internal/supports/OnEnterSupport.java
@@ -3,20 +3,14 @@
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- *
+ * <p>
  * SPDX-License-Identifier: EPL-2.0
- *
+ * <p>
  * Contributors:
  * Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
  * Sebastian Thomschke (Vegard IT GmbH) - add previousLineText support
  */
 package org.eclipse.tm4e.languageconfiguration.internal.supports;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import org.eclipse.jdt.annotation.Nullable;
 import org.eclipse.tm4e.languageconfiguration.internal.model.CharacterPair;
@@ -24,6 +18,11 @@ import org.eclipse.tm4e.languageconfiguration.internal.model.EnterAction;
 import org.eclipse.tm4e.languageconfiguration.internal.model.EnterAction.IndentAction;
 import org.eclipse.tm4e.languageconfiguration.internal.model.OnEnterRule;
 import org.eclipse.tm4e.languageconfiguration.internal.utils.RegExpUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * On enter support.
@@ -33,107 +32,107 @@ import org.eclipse.tm4e.languageconfiguration.internal.utils.RegExpUtils;
  */
 public class OnEnterSupport {
 
-	private static final List<CharacterPair> DEFAULT_BRACKETS = List.of(
-			new CharacterPair("(", ")"), //$NON-NLS-1$ //$NON-NLS-2$
-			new CharacterPair("{", "}"), //$NON-NLS-1$ //$NON-NLS-2$
-			new CharacterPair("[", "]")); //$NON-NLS-1$ //$NON-NLS-2$
+    private static final List<CharacterPair> DEFAULT_BRACKETS = List.of(
+            new CharacterPair("(", ")"), //$NON-NLS-1$ //$NON-NLS-2$
+            new CharacterPair("{", "}"), //$NON-NLS-1$ //$NON-NLS-2$
+            new CharacterPair("[", "]")); //$NON-NLS-1$ //$NON-NLS-2$
 
-	private final List<ProcessedBracketPair> brackets;
-	private final List<OnEnterRule> regExpRules;
+    private final List<ProcessedBracketPair> brackets;
+    private final List<OnEnterRule> regExpRules;
 
-	public OnEnterSupport(@Nullable final List<CharacterPair> brackets, @Nullable final List<OnEnterRule> regExpRules) {
-		this.brackets = (brackets != null ? brackets : DEFAULT_BRACKETS)
-				.stream()
-				.filter(Objects::nonNull)
-				.map(ProcessedBracketPair::new)
-				.collect(Collectors.toList());
+    public OnEnterSupport(@Nullable final List<CharacterPair> brackets, @Nullable final List<OnEnterRule> regExpRules) {
+        this.brackets = (brackets != null ? brackets : DEFAULT_BRACKETS)
+                .stream()
+                .filter(t -> t != null)
+                .map(ProcessedBracketPair::new)
+                .collect(Collectors.toList());
 
-		this.regExpRules = regExpRules != null ? regExpRules : Collections.emptyList();
-	}
+        this.regExpRules = regExpRules != null ? regExpRules : Collections.emptyList();
+    }
 
-	public @Nullable EnterAction onEnter(
-			// TODO autoIndent: EditorAutoIndentStrategy,
-			final String previousLineText,
-			final String beforeEnterText,
-			final String afterEnterText) {
-		// (1): `regExpRules`
-		// if (autoIndent >= EditorAutoIndentStrategy.Advanced) {
-		for (final OnEnterRule rule : regExpRules) {
-			if (!rule.beforeText.matchesPartially(beforeEnterText))
-				continue;
+    public @Nullable EnterAction onEnter(
+            // TODO autoIndent: EditorAutoIndentStrategy,
+            final String previousLineText,
+            final String beforeEnterText,
+            final String afterEnterText) {
+        // (1): `regExpRules`
+        // if (autoIndent >= EditorAutoIndentStrategy.Advanced) {
+        for (final OnEnterRule rule : regExpRules) {
+            if (!rule.beforeText.matchesPartially(beforeEnterText))
+                continue;
 
-			final var afterTextPattern = rule.afterText;
-			if (afterTextPattern != null && !afterTextPattern.matchesPartially(afterEnterText))
-				continue;
+            final var afterTextPattern = rule.afterText;
+            if (afterTextPattern != null && !afterTextPattern.matchesPartially(afterEnterText))
+                continue;
 
-			final var previousLinePattern = rule.previousLineText;
-			if (previousLinePattern != null && !previousLinePattern.matchesPartially(previousLineText))
-				continue;
+            final var previousLinePattern = rule.previousLineText;
+            if (previousLinePattern != null && !previousLinePattern.matchesPartially(previousLineText))
+                continue;
 
-			return rule.action;
-		}
+            return rule.action;
+        }
 
-		// (2): Special indent-outdent
-		// if (autoIndent >= EditorAutoIndentStrategy.Brackets) {
-		if (!beforeEnterText.isEmpty() && !afterEnterText.isEmpty()) {
-			for (final ProcessedBracketPair bracket : brackets) {
-				if (bracket.matchOpen(beforeEnterText) && bracket.matchClose(afterEnterText)) {
-					return new EnterAction(IndentAction.IndentOutdent);
-				}
-			}
-		}
+        // (2): Special indent-outdent
+        // if (autoIndent >= EditorAutoIndentStrategy.Brackets) {
+        if (!beforeEnterText.isEmpty() && !afterEnterText.isEmpty()) {
+            for (final ProcessedBracketPair bracket : brackets) {
+                if (bracket.matchOpen(beforeEnterText) && bracket.matchClose(afterEnterText)) {
+                    return new EnterAction(IndentAction.IndentOutdent);
+                }
+            }
+        }
 
-		// (3): Open bracket based logic
-		// if (autoIndent >= EditorAutoIndentStrategy.Brackets) {
-		if (!beforeEnterText.isEmpty()) {
-			for (final ProcessedBracketPair bracket : brackets) {
-				if (bracket.matchOpen(beforeEnterText)) {
-					return new EnterAction(IndentAction.Indent);
-				}
-			}
-		}
+        // (3): Open bracket based logic
+        // if (autoIndent >= EditorAutoIndentStrategy.Brackets) {
+        if (!beforeEnterText.isEmpty()) {
+            for (final ProcessedBracketPair bracket : brackets) {
+                if (bracket.matchOpen(beforeEnterText)) {
+                    return new EnterAction(IndentAction.Indent);
+                }
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	private static final class ProcessedBracketPair {
+    private static final class ProcessedBracketPair {
 
-		private static final Pattern B_REGEXP = Pattern.compile("\\B"); //$NON-NLS-1$
+        private static final Pattern B_REGEXP = Pattern.compile("\\B"); //$NON-NLS-1$
 
-		private final @Nullable Pattern openRegExp;
-		private final @Nullable Pattern closeRegExp;
+        private final @Nullable Pattern openRegExp;
+        private final @Nullable Pattern closeRegExp;
 
-		private ProcessedBracketPair(final CharacterPair charPair) {
-			openRegExp = createOpenBracketRegExp(charPair.open);
-			closeRegExp = createCloseBracketRegExp(charPair.close);
-		}
+        private ProcessedBracketPair(final CharacterPair charPair) {
+            openRegExp = createOpenBracketRegExp(charPair.open);
+            closeRegExp = createCloseBracketRegExp(charPair.close);
+        }
 
-		private boolean matchOpen(final String beforeEnterText) {
-			return openRegExp != null && openRegExp.matcher(beforeEnterText).find();
-		}
+        private boolean matchOpen(final String beforeEnterText) {
+            return openRegExp != null && openRegExp.matcher(beforeEnterText).find();
+        }
 
-		private boolean matchClose(final String afterEnterText) {
-			return closeRegExp != null && closeRegExp.matcher(afterEnterText).find();
-		}
+        private boolean matchClose(final String afterEnterText) {
+            return closeRegExp != null && closeRegExp.matcher(afterEnterText).find();
+        }
 
-		private static @Nullable Pattern createOpenBracketRegExp(final String bracket) {
-			final var str = new StringBuilder(RegExpUtils.escapeRegExpCharacters(bracket));
-			final var c = String.valueOf(str.charAt(0));
-			if (!B_REGEXP.matcher(c).find()) {
-				str.insert(0, "\\b"); //$NON-NLS-1$
-			}
-			str.append("\\s*$"); //$NON-NLS-1$
-			return RegExpUtils.create(str.toString());
-		}
+        private static @Nullable Pattern createOpenBracketRegExp(final String bracket) {
+            final var str = new StringBuilder(RegExpUtils.escapeRegExpCharacters(bracket));
+            final var c = String.valueOf(str.charAt(0));
+            if (!B_REGEXP.matcher(c).find()) {
+                str.insert(0, "\\b"); //$NON-NLS-1$
+            }
+            str.append("\\s*$"); //$NON-NLS-1$
+            return RegExpUtils.create(str.toString());
+        }
 
-		private static @Nullable Pattern createCloseBracketRegExp(final String bracket) {
-			final var str = new StringBuilder(RegExpUtils.escapeRegExpCharacters(bracket));
-			final var c = String.valueOf(str.charAt(str.length() - 1));
-			if (!B_REGEXP.matcher(c).find()) {
-				str.append("\\b"); //$NON-NLS-1$
-			}
-			str.insert(0, "^\\s*"); //$NON-NLS-1$
-			return RegExpUtils.create(str.toString());
-		}
-	}
+        private static @Nullable Pattern createCloseBracketRegExp(final String bracket) {
+            final var str = new StringBuilder(RegExpUtils.escapeRegExpCharacters(bracket));
+            final var c = String.valueOf(str.charAt(str.length() - 1));
+            if (!B_REGEXP.matcher(c).find()) {
+                str.append("\\b"); //$NON-NLS-1$
+            }
+            str.insert(0, "^\\s*"); //$NON-NLS-1$
+            return RegExpUtils.create(str.toString());
+        }
+    }
 }
