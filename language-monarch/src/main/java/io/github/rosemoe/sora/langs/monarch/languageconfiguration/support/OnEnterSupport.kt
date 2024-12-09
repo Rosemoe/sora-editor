dbdecfbeff15df6f033eaaa66bd5ee4221761f70
Rosemoe <2073412493@qq.com>
2024-12-09 16:26:45 +0800

    chore: add scopes for EPL-licensed files and update copyright for monarch source files

--- language-monarch/src/main/java/io/github/rosemoe/sora/langs/monarch/languageconfiguration/support/OnEnterSupport.kt
+++ language-monarch/src/main/java/io/github/rosemoe/sora/langs/monarch/languageconfiguration/support/OnEnterSupport.kt
@@ -1,25 +1,15 @@
 /*******************************************************************************
- *    sora-editor - the awesome code editor for Android
- *    https://github.com/Rosemoe/sora-editor
- *    Copyright (C) 2020-2024  Rosemoe
+ * Copyright (c) 2015-2017 Angelo ZERR.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * <p>
+ * SPDX-License-Identifier: EPL-2.0
+ * <p>
+ * Contributors:
+ * Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ * Sebastian Thomschke (Vegard IT GmbH) - add previousLineText support
  *
- *     This library is free software; you can redistribute it and/or
- *     modify it under the terms of the GNU Lesser General Public
- *     License as published by the Free Software Foundation; either
- *     version 2.1 of the License, or (at your option) any later version.
- *
- *     This library is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *     Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public
- *     License along with this library; if not, write to the Free Software
- *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- *     USA
- *
- *     Please contact Rosemoe by email 2073412493@qq.com if you need
- *     additional information or have any questions
  ******************************************************************************/
 
 package io.github.rosemoe.sora.langs.monarch.languageconfiguration.support
