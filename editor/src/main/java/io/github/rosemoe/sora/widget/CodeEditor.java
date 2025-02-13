7ccd26154ce22b239751577cd7ae94399eada848
Rosemoe <2073412493@qq.com>
2025-02-13 14:08:08 +0800

    fix(editor): negative position Y in `CodeEditor#getPointPositionOnScreen` leads to crash (fix #655)

--- editor/src/main/java/io/github/rosemoe/sora/widget/CodeEditor.java
+++ editor/src/main/java/io/github/rosemoe/sora/widget/CodeEditor.java
@@ -2234,8 +2234,9 @@ public class CodeEditor extends View implements ContentListener, Formatter.Forma
         y = Math.max(0, y);
         var stuckLines = renderer.lastStuckLines;
         if (stuckLines != null) {
-            if (y < stuckLines.size() * getRowHeight()) {
-                var index = (int) (y / getRowHeight());
+            // position Y maybe negative
+            var index = (int) Math.max(0, (y / getRowHeight()));
+            if (y < stuckLines.size() * getRowHeight() && index < stuckLines.size()) {
                 return getPointPosition(x, layout.getCharLayoutOffset(stuckLines.get(index).startLine, 0)[0] - getRowHeight() / 2f);
             }
         }
