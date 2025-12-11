63b5efc2e5029da83a1e86e4cae957af433873fc
Rosemoe <2073412493@qq.com>
2025-12-11 22:08:03 +0800

    feat(app): add more bracket captures for tree-sitter Java

--- app/src/main/assets/tree-sitter-queries/java/brackets.scm
+++ app/src/main/assets/tree-sitter-queries/java/brackets.scm
@@ -12,3 +12,31 @@
 (array_initializer
   "{" @editor.brackets.open
   "}" @editor.brackets.close)
+
+(class_body
+  "{" @editor.brackets.open
+  "}" @editor.brackets.close)
+
+(enum_body
+  "{" @editor.brackets.open
+  "}" @editor.brackets.close)
+
+(interface_body
+  "{" @editor.brackets.open
+  "}" @editor.brackets.close)
+
+(annotation_type_body
+  "{" @editor.brackets.open
+  "}" @editor.brackets.close)
+
+(dimensions_expr
+  "[" @editor.brackets.open
+  "]" @editor.brackets.close)
+
+(array_access
+  "[" @editor.brackets.open
+  "]" @editor.brackets.close)
+
+(dimensions
+  "[" @editor.brackets.open
+  "]" @editor.brackets.close)
