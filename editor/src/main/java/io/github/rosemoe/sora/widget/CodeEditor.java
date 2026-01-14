ae655461c8a556df898c24848af5c391703909b0
Rosemoe <2073412493@qq.com>
2026-01-14 13:56:43 +0800

    fix(editor): cursor information generated before composing text is adjusted (fix #782)

--- editor/src/main/java/io/github/rosemoe/sora/widget/CodeEditor.java
+++ editor/src/main/java/io/github/rosemoe/sora/widget/CodeEditor.java
@@ -5290,11 +5290,6 @@ public class CodeEditor extends View implements ContentListener, Formatter.Forma
         renderer.buildMeasureCacheForLines(startLine, startLine + 1);
         checkForRelayout();
 
-        if (!waitForNextChange) {
-            updateCursorAnchor();
-            ensureSelectionVisible();
-            touchHandler.hideInsertHandle();
-        }
         if (editable && !cursor.isSelected() && !waitForNextChange && !inputConnection.composingText.isComposing() && acceptsComposingText()) {
             cursorAnimator.markEndPos();
             cursorAnimator.start();
@@ -5304,7 +5299,11 @@ public class CodeEditor extends View implements ContentListener, Formatter.Forma
         dispatchEvent(new ContentChangeEvent(this, ContentChangeEvent.ACTION_DELETE, start, end, deletedContent, text.isUndoManagerWorking()));
         onSelectionChanged(SelectionChangeEvent.CAUSE_TEXT_MODIFICATION);
 
-        updateCursor();
+        if (!waitForNextChange) {
+            updateCursor();
+            ensureSelectionVisible();
+            touchHandler.hideInsertHandle();
+        }
     }
 
     @Override
