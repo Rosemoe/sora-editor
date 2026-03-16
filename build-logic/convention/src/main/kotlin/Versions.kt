0fdd4a8f08dbaf1e6b91353fc264c1ddf0b7f51f
Rosemoe <2073412493@qq.com>
2026-03-16 22:30:49 +0800

    chore: bump version to 0.24.5

--- build-logic/convention/src/main/kotlin/Versions.kt
+++ build-logic/convention/src/main/kotlin/Versions.kt
@@ -24,8 +24,8 @@
 @SuppressWarnings("unused")
 object Versions {
     // Project versions
-    private const val version = "0.24.4"
-    const val versionCode = 93
+    private const val version = "0.24.5"
+    const val versionCode = 94
 
     val appVersionName by lazy {
         if (CI.isCiBuild) {
