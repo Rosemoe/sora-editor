91051f3c0f16c7d3bf5c3360f2421c0a08cba87d
Rosemoe <2073412493@qq.com>
2026-03-26 13:48:09 +0800

    chore: fix comment grammar

--- app/src/main/assets/tree-sitter-queries/java/blocks.scm
+++ app/src/main/assets/tree-sitter-queries/java/blocks.scm
@@ -1,6 +1,6 @@
 ; Code block patterns for editor
 ; Capture names for scopes does not matter much in sora-editor implementation, you may use 'abc', 'test.xyz', etc.
-; General, editor considers the captured node's region as code block region.
+; Generally, editor considers the captured node's region as code block region.
 ; However, capture name with '.marked' suffix is special. The last terminal node's start position in the capture will be the end position of the block
 ; 'terminal node' refers to a node without children
 
