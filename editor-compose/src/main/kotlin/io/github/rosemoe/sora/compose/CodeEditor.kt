21bd4a04245155c5e7cf5f45b3a9f5a654b3f12e
Vivek <itsvks19@gmail.com>
2026-05-01 19:15:27 +0530

    feat: add accessibility semantics

--- editor-compose/src/main/kotlin/io/github/rosemoe/sora/compose/CodeEditor.kt
+++ editor-compose/src/main/kotlin/io/github/rosemoe/sora/compose/CodeEditor.kt
@@ -34,8 +34,28 @@ import androidx.compose.ui.UiComposable
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.input.pointer.PointerIcon
 import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.semantics.ScrollAxisRange
+import androidx.compose.ui.semantics.accessibilityClassName
+import androidx.compose.ui.semantics.copyText
+import androidx.compose.ui.semantics.cutText
+import androidx.compose.ui.semantics.editableText
+import androidx.compose.ui.semantics.horizontalScrollAxisRange
+import androidx.compose.ui.semantics.isEditable
+import androidx.compose.ui.semantics.onLongClick
+import androidx.compose.ui.semantics.pasteText
+import androidx.compose.ui.semantics.scrollBy
+import androidx.compose.ui.semantics.scrollByOffset
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.setText
+import androidx.compose.ui.semantics.stateDescription
+import androidx.compose.ui.semantics.textSelectionRange
+import androidx.compose.ui.semantics.verticalScrollAxisRange
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.rosemoe.sora.compose.internal.CodeEditorImpl
+import io.github.rosemoe.sora.event.SelectionChangeEvent
+import io.github.rosemoe.sora.text.TextUtils
 
 /**
  * Composable for displaying and editing code.
@@ -66,6 +86,75 @@ fun CodeEditor(
                     .matchParentSize()
                     .clipToBounds()
                     .pointerHoverIcon(PointerIcon.Text, overrideDescendants = true)
+                    .semantics(mergeDescendants = true) {
+                        stateDescription = if (enabled) "enabled" else "disabled"
+
+                        if (enabled) {
+                            val maxLength = state.props.maxAccessibilityTextLength
+                            if (maxLength > 0) {
+                                isEditable = state.isEditable
+                                textSelectionRange = TextRange(state.cursor.left, state.cursor.right)
+                                accessibilityClassName = "CodeEditor"
+                                //text = AnnotatedString(TextUtils.trimToSize(state.text, maxLength).toString())
+                                //inputText = AnnotatedString(TextUtils.trimToSize(state.text, maxLength).toString())
+                                editableText = AnnotatedString(TextUtils.trimToSize(state.text, maxLength).toString())
+
+                                copyText {
+                                    state.copyText()
+                                    true
+                                }
+
+                                cutText {
+                                    state.cutText()
+                                    true
+                                }
+
+                                pasteText {
+                                    state.pasteText()
+                                    true
+                                }
+
+                                setText { text ->
+                                    state.setText(text)
+                                    true
+                                }
+                            }
+
+                            onLongClick { true }
+
+                            verticalScrollAxisRange = ScrollAxisRange(
+                                value = { state.scrollY.toFloat() },
+                                maxValue = { state.maxScrollY.toFloat() },
+                                reverseScrolling = false
+                            )
+
+                            horizontalScrollAxisRange = ScrollAxisRange(
+                                value = { state.scrollX.toFloat() },
+                                maxValue = { state.maxScrollX.toFloat() },
+                                reverseScrolling = false
+                            )
+
+//                            scrollBy { x, y ->
+//                                val (line, column) = state.getPointPosition(
+//                                    state.scrollX + x,
+//                                    state.scrollY + y
+//                                )
+//                                state.delegate.touchHandler.scrollBy(x, y, true)
+//                                state.setSelection(line, column, SelectionChangeEvent.CAUSE_KEYBOARD_OR_CODE)
+//                                true
+//                            }
+
+                            scrollByOffset { offset ->
+                                val (line, column) = state.getPointPosition(
+                                    state.scrollX + offset.x,
+                                    state.scrollY + offset.y
+                                )
+                                state.delegate.touchHandler.scrollBy(offset.x, offset.y, true)
+                                state.setSelection(line, column, SelectionChangeEvent.CAUSE_KEYBOARD_OR_CODE)
+                                offset
+                            }
+                        }
+                    }
             )
         }
     }
