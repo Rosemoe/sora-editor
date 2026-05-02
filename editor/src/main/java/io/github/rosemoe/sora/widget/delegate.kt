4672e20737bed8332a00dd97892c72b9a3329887
Vivek <itsvks19@gmail.com>
2026-05-02 10:14:25 +0530

    feat: add helper extensions to CodeEditorDelegate for editor view access

--- editor/src/main/java/io/github/rosemoe/sora/widget/delegate.kt
+++ editor/src/main/java/io/github/rosemoe/sora/widget/delegate.kt
@@ -24,4 +24,20 @@
 
 package io.github.rosemoe.sora.widget
 
+/**
+ * Returns the [CodeEditor] instance associated with this delegate, or null if the editor
+ * is not an instance of [CodeEditor].
+ *
+ * Note: When using the editor within a Jetpack Compose environment (e.g., via `CodeEditor`
+ * composable), this method will return **null** because the internal implementation uses
+ * a different view structure. In standard View-based layouts, this will return the
+ * non-null [CodeEditor] instance.
+ */
 fun CodeEditorDelegate.asCodeEditor(): CodeEditor? = host.attachedView as? CodeEditor
+
+/**
+ * Returns true if the delegate is attached to a [CodeEditor] view.
+ * This will return false in Jetpack Compose environments.
+ */
+val CodeEditorDelegate.isViewEditor: Boolean
+    get() = host.attachedView is CodeEditor
